2bed250e517305215cf63a74b2a5d61598aa1987
IFPE - P3 <hudsondabalakagt@gmail.com>
2019-09-12 11:41:09 -0300

    pagina de login adicionada

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  @ViewChild(IonSlides) slides: IonSlides;
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  segmentChanged(event: any) {
+    if (event.detail.value === 'login') {
+      this.slides.slidePrev();
+    } else {
+      this.slides.slideNext();
+    }
+  }
 }
