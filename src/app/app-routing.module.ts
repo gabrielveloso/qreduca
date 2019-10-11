691f6f69e1fd042f8567abae0760e57f423947b4
Hudson <hudsondabalakagt@gmail.com>
2019-10-11 09:39:00 -0300

    fix login autentication e add sistema de protecao de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'principal',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
-  { path: 'pqrcode', loadChildren: './pages/pqrcode/pqrcode.module#PQRCodePageModule' },
-  { path: 'minformacoes', loadChildren: './pages/minformacoes/minformacoes.module#MInformacoesPageModule' },
-  { path: 'sobre', loadChildren: './pages/sobre/sobre.module#SobrePageModule' },
-  { path: 'principal', loadChildren: './pages/principal/principal.module#PrincipalPageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'lista-itens', loadChildren: './pages/lista-itens/lista-itens.module#ListaItensPageModule' },
+  { path: 'pqrcode', loadChildren: './pages/pqrcode/pqrcode.module#PQRCodePageModule' ,canActivate: [AuthGuard] },
+  { path: 'minformacoes', loadChildren: './pages/minformacoes/minformacoes.module#MInformacoesPageModule' ,canActivate: [AuthGuard]},
+  { path: 'sobre', loadChildren: './pages/sobre/sobre.module#SobrePageModule' ,canActivate: [AuthGuard]},
+  { path: 'principal', loadChildren: './pages/principal/principal.module#PrincipalPageModule' ,canActivate: [AuthGuard]},
+
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule',canActivate: [LoginGuard] },
+
+  { path: 'lista-itens', loadChildren: './pages/lista-itens/lista-itens.module#ListaItensPageModule' ,canActivate: [AuthGuard]},
   { path: 'adicionar-item', loadChildren: './pages/adicionar-item/adicionar-item.module#AdicionarItemPageModule' },
-  { path: 'detalhamento-item/:id', loadChildren: './pages/detalhamento-item/detalhamento-item.module#DetalhamentoItemPageModule' }
+  { path: 'detalhamento-item/:id', loadChildren: './pages/detalhamento-item/detalhamento-item.module#DetalhamentoItemPageModule' ,canActivate: [AuthGuard]}
 
 ];
 
--- src/app/pages/principal/principal.page.ts
+++ src/app/pages/principal/principal.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { LoadingController, ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-principal',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal.page.scss'],
 })
 export class PrincipalPage implements OnInit {
+  private loading: any;
 
-  constructor() { }
+
+  constructor(
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
+  ) { }
 
   ngOnInit() {
   }
 
+  async logout() {
+    await this.presentLoading();
+
+    try {
+      await this.authService.logout();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.loading.dismiss();
+    }
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({ message: 'Aguarde...' });
+    return this.loading.present();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({ message, duration: 2000 });
+    toast.present();
+  }
 }
