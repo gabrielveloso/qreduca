ba3d18c4b27482bae8737b6e1f2b8b861b22e558
Gabriel Veloso <gabrielveloso2@gmail.com>
2019-11-05 23:48:13 -0300

    lista de marcadores

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Platform, LoadingController } from '@ionic/angular';
-import { Environment, GoogleMaps , GoogleMap, GoogleMapOptions, GoogleMapsEvent, MyLocation, GoogleMapsAnimation } from '@ionic-native/google-maps';
+import { Environment, GoogleMaps , GoogleMap, GoogleMapOptions, GoogleMapsEvent, MyLocation, GoogleMapsAnimation, LatLng, MarkerIcon } from '@ionic-native/google-maps';
 import { map } from 'rxjs/operators';
 import { Icon } from 'ionicons/dist/types/icon/icon';
+import { ItemService } from 'src/app/services/item.service';
+import { Subscription } from 'rxjs';
+import { Item } from 'src/app/interfaces/item';
 
 @Component({
   selector: 'app-mapa',
@@ -13,11 +16,18 @@ export class MapaPage implements OnInit {
   @ViewChild('map',{static:true}) mapElement: any;
   private loading: any;
   private map: GoogleMap;
+  private itemSubscription: Subscription;
+  public itens = new Array<Item>();
 
   constructor(
     private Platform: Platform,
-    private loadingCtrl: LoadingController
-  ) { }
+    private loadingCtrl: LoadingController,
+    private itemService: ItemService
+  ) { 
+    this.itemSubscription = this.itemService.getItens().subscribe(data =>{
+      this.itens = data
+    })
+  }
 
   ngOnInit() {
     this.mapElement = this.mapElement.nativeElement;
@@ -32,8 +42,8 @@ export class MapaPage implements OnInit {
     await this.loading.present();
     
     Environment.setEnv({
-      'API_KEY_FOR_BROWSER_RELEASE': '',
-      'API_KEY_FOR_BROWSER_DEBUG': ''
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyCSvWuMMyntTz1oSpw4WbzaRznOUSuo5C8',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyCSvWuMMyntTz1oSpw4WbzaRznOUSuo5C8'
     });
 
     const mapOptions: GoogleMapOptions = {
@@ -57,10 +67,38 @@ export class MapaPage implements OnInit {
     try{
       const myLocation: MyLocation = await this.map.getMyLocation();
 
+     
+      35.594472
+      let myposition: LatLng = new LatLng( -8.6827393, -35.594472);
       await this.map.moveCamera({
-        target: myLocation.latLng,
-        zoom: 18
+        target: myposition,
+        zoom: 16
       });
+
+      console.log(myLocation.latLng);
+      for(let item of this.itens){
+        
+          if (item.latitude != undefined){            
+            let position: LatLng = new LatLng(Number(item.latitude), Number(item.longitude));
+            console.log(position)
+            
+            const icone: MarkerIcon = {
+              url: 'assets/img/pin1.png',
+              size: {
+                width: 24,
+                height: 24
+              }
+              
+            };
+            
+            this.map.addMarkerSync({
+              title: 'Origem',              
+              icon:'#0F0',
+              animation: GoogleMapsAnimation.DROP,
+              position: position
+            });
+          }          
+      }
 
       this.map.addMarkerSync({
         title: 'Origem',
@@ -68,11 +106,17 @@ export class MapaPage implements OnInit {
         animation: GoogleMapsAnimation.DROP,
         position: myLocation.latLng
       });
+
+      
       console.log(myLocation);
     } catch(error){
       console.error(error);
     }finally{
       this.loading.dismiss();
     }
+  }
+
+  ngOnDestroy(){
+    this.itemSubscription.unsubscribe();
   }
 }
