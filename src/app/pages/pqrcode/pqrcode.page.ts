c1db59fea23d4da25bf84317284586d4f50b5047
Thiago Bezerra <thiagoprazeres@gmail.com>
2019-12-05 01:08:07 -0300

    loading qrcode scanner

--- src/app/pages/pqrcode/pqrcode.page.ts
+++ src/app/pages/pqrcode/pqrcode.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 import { NavController } from '@ionic/angular';
 
@@ -7,95 +7,58 @@ import { NavController } from '@ionic/angular';
   templateUrl: './pqrcode.page.html',
   styleUrls: ['./pqrcode.page.scss'],
 })
-export class PQRCodePage implements OnInit {
+export class PQRCodePage implements OnInit, OnDestroy {
 
   encodedData = '';
   QRSCANNED_DATA: string;
   isOn = false;
   scannedData: {};
-  constructor(private qrScanCtrl: QRScanner,
-    private navCtrl: NavController) {
-    
-   }
+  constructor(private qrScanCtrl: QRScanner, private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.goToQrScan();
   }
 
-  ligar(){
-    if(this.isOn){
-      //this.hideCamera();
-      this.closeScanner();
-      this.isOn = false;
-    }else{
-      this.goToQrScan();
-    }
+  ngOnDestroy() {
+    this.closeScanner();
   }
 
   goToQrScan() {
-    this.isOn = true;
-    this.qrScanCtrl.prepare()
-    .then((status: QRScannerStatus) => {
+    this.qrScanCtrl.prepare().then((status: QRScannerStatus) => {
+      console.log('status.canChangeCamera', status.canChangeCamera);
+      this.isOn = true;
       if (status.authorized) {
         // camera permission was granted
- 
-        //this.showCamera();
+
         // start scanning
-        let scanSub = this.qrScanCtrl.scan().subscribe((text: any) => {
+        const scanSub = this.qrScanCtrl.scan().subscribe((text: any) => {
           console.log('Scanned something', text);
-          console.log(text)
-          //this.scannedData = text;
-          this.navCtrl.navigateForward('/detalhamento-item/'+text.result);
- 
+          console.log(text);
+          // this.scannedData = text;
+          this.navCtrl.navigateForward('/detalhamento-item/' + text.result);
+
           this.closeScanner(); // hide camera preview
-          scanSub.unsubscribe(); // stop scanning          
-          //this.hideCamera();
-         
+          scanSub.unsubscribe(); // stop scanning
+
         });
         this.qrScanCtrl.show();
- 
+
       } else if (status.denied) {
-        console.log('Scanned denied')
+        alert('Permissão da câmera negada :(');
         // camera permission was permanently denied
         // you must use QRScanner.openSettings() method to guide the user to the settings page
         // then they can grant the permission from there
       } else {
-        console.log('error denied')
+        alert('Permissão foi negada, mas não permanentemente. Você pode pedir permissão novamente mais tarde.');
         // permission was denied, but not permanently. You can ask for permission again at a later time.
       }
-   })
-   .catch((e: any) => console.log('Error is', e));
+    }).catch((e: any) => console.log('Error is', e));
   }
-
-  showCamera() {
-    setTimeout(() => {
-        window.document.querySelectorAll('ion-content')
-              .forEach(element => {
-                  const element1 = element.shadowRoot.querySelector('style');
-                  if(element1 != null){
-                    element1.innerHTML = element1.innerHTML
-                                               .replace('--background:var(--ion-background-color,#fff);', '--background: transparent');
-                  }
-              });
-    }, 300);
-}
-
-hideCamera() {
-    window.document.querySelectorAll('ion-content')
-          .forEach(element => {
-              const element1 = element.shadowRoot.querySelector('style');
-              if(element1 != null){
-                element1.innerHTML = element1.innerHTML
-                                           .replace('--background: transparent', '--background:var(--ion-background-color,#fff);');
-              }
-          });
-}
 
   closeScanner() {
     this.isOn = false;
     this.qrScanCtrl.hide();
     this.qrScanCtrl.destroy();
   }
-
-
 
 }
